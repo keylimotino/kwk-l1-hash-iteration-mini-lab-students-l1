174ea0b9948e68d4b1e1d258e3286419d658486f
keylimotino <stephanie48758@gmail.com>
2018-06-06 19:59:33 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -28,6 +28,7 @@ def iterate_through_hash
   :Beijing => "2008",
   :London => "2012"
   }
+  summer_olympics[:Atlanta] = "1996"
   summer_olympics.each do |place, year|
     puts "The #{place} summer olympics took place in #{year}."
   end
