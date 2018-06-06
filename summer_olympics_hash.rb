cb2f9b7b96bba0bfc1e4bc8e3f18583ec55affe9
keylimotino <stephanie48758@gmail.com>
2018-06-06 19:55:52 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -22,6 +22,12 @@ def add_a_key_value_pair
 end
 
 def iterate_through_hash
+  summer_olympics = {
+  :Sydney => "2000",
+  :Athens => "2004",
+  :Beijing => "2008",
+  :London => "2012"
+  }
   summer_olympics.each do |place, year|
     puts "The #{place} summer olympics took place in #{year}"
   end
