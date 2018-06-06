4ed66ad5815c64f0739043e97f2c83c162a4ca63
keylimotino <stephanie48758@gmail.com>
2018-06-06 19:51:05 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,10 +1,10 @@
 
 def create_olympics_hash
   summer_olympics = {
-  :Sydney ==> "2000",
-  :Athens ==> "2004",
-  :Beijing ==> "2008",
-  :London ==> "2012"
+  :Sydney => "2000",
+  :Athens => "2004",
+  :Beijing => "2008",
+  :London => "2012"
   }
   puts summer_olympics
 end
@@ -12,10 +12,10 @@ end
 
 def add_a_key_value_pair
   summer_olympics = {
-  :Sydney ==> "2000",
-  :Athens ==> "2004",
-  :Beijing ==> "2008",
-  :London ==> "2012"
+  :Sydney => "2000",
+  :Athens => "2004",
+  :Beijing => "2008",
+  :London => "2012"
   }
   summer_olympics[:Atlanta] = 1996
   puts summer_olympics
