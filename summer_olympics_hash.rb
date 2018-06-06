24d972887362627a240539d259fc2dd46a5c5f8d
keylimotino <stephanie48758@gmail.com>
2018-06-06 20:00:13 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -35,5 +35,12 @@ def iterate_through_hash
 end
 
 def iterate_through_keys
+  summer_olympics = {
+  :Sydney => "2000",
+  :Athens => "2004",
+  :Beijing => "2008",
+  :London => "2012"
+  }
+  summer_olympics[:Atlanta] = "1996"
   upcased_cities = summer_olympics.each.upcase
 end
